6a47ad729ea5eff39677ca7187b9455751e77907
flapperz <flap.atyz@gmail.com>
2024-05-24 15:27:36 +0700

    Restructure

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("search-input");
   const tabsList = document.getElementById("tabs-list");
+
   let selectIndex = 0;
   let isSelect = true;
 
@@ -9,16 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 100);
 
   let tabs = [];
-  let searchIndex2tabId = [];
-  const items = [];
+  let currentSearchTabId = [];
+  const tabsLi = [];
+
+  function createTabItems(tabs) {}
+
+  function renderTabItems(tabs) {}
 
   chrome.tabs.query({}, (queriedTabs) => {
     tabs = queriedTabs;
-    let titleText = "Open v1 tabs:";
+    let titleText = "Open tabs:";
     document.getElementById("title").textContent =
       titleText + ` ${tabs.length}`;
     tabs.forEach((tab) => {
       const li = document.createElement("li");
+      li.className = "tabItem-unfocus";
+
       const img = document.createElement("img");
       const favIconUrl = `https://www.google.com/s2/favicons?sz=64&domain_url=${tab.url}`;
       // img.src = tab.favIconUrl ? tab.favIconUrl : "icons/globe-16x16.png";
@@ -29,29 +36,29 @@ document.addEventListener("DOMContentLoaded", function () {
       const span = document.createElement("span");
       span.textContent = tab.title;
 
-      li.className = "tabli";
       li.appendChild(img);
       li.appendChild(span);
       tabsList.appendChild(li);
-      items.push(li);
+      tabsLi.push(li);
     });
 
-    searchIndex2tabId = tabs.map((tab) => tab.id);
+    currentSearchTabId = tabs.map((tab) => tab.id);
+    if (tabsLi.length) tabsLi[0].className = "tabItem-focus";
   });
 
   searchInput.addEventListener("input", () => {
     const searchTerm = searchInput.value.toLowerCase();
     const regex = new RegExp(searchTerm.split("").join(".*"), "i");
 
-    searchIndex2tabId = [];
+    currentSearchTabId = [];
     searchIndex = 0;
-    for (var i = 0; i < items.length; i++) {
-      const li = items[i];
+    for (var i = 0; i < tabsLi.length; i++) {
+      const li = tabsLi[i];
       const tabtitle = li.children[1].textContent;
 
       if (regex.test(tabtitle.toLowerCase())) {
         li.style.display = "";
-        searchIndex2tabId.push(tabs[i].id);
+        currentSearchTabId.push(tabs[i].id);
 
         searchIndex++;
       } else {
@@ -62,14 +69,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   searchInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-      if (searchIndex2tabId.length) {
-        chrome.tabs.update(searchIndex2tabId[selectIndex], { active: true });
+      if (currentSearchTabId.length) {
+        chrome.tabs.update(currentSearchTabId[selectIndex], { active: true });
       }
     }
   });
 
   // TODO: Select tab
   // TODO: VIM navigate key (Key chord ctrl+direction)
+  // TODO: close tab button
   // TODO: highlight match like in VSCode
-  // TODO: make UI native
+  // TODO: history
+  // TODO: Click to goto
+  // TODO: Use Fuse instead of REGEX
 });
